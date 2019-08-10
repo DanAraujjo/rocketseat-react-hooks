bfcd0aa9f2296ed771f05d9fdd5ab4f5c33ed6f3
Daniel Araujo <dan.araujjo@gmail.com>
2019-08-09 22:22:13 -0300

    Hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>React Hooks</h1>;
+  const [tech, setTech] = useState(['NodeJs', 'ReactJs']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+  return (
+    <>
+      <h1>React Hooks</h1>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adcionar
+      </button>
+      <br />
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
